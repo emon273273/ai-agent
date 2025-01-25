2fd115370ea498aa2bd11ff3609cda714ed638bf
emon273273 <emon.cse273@gmail.com>
2025-01-25 16:54:30 +0600

    context reasoning  related chat implemented

--- chat_model_with_context/chat_model_with_context.py
+++ chat_model_with_context/chat_model_with_context.py
@@ -1,15 +1,42 @@
-from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
-from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_groq import ChatGroq
+
 load_dotenv()
 
 
-llm=ChatGroq(model="llama-3.2-90b-vision-preview")
+llm = ChatGroq(
+    # model="mixtral-8x7b-32768",
+    model="llama-3.2-90b-vision-preview",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
 
-message=[
-    SystemMessage("You are a expert Cse Enginner"),
-    HumanMessage("Give me a c code of find prime number of 1 to 100")
-]
 
-result=llm.invoke(message)
-print(result.content)
+# message=[
+#     SystemMessage("You are an expert computer science and engineering (CSE) professional. Your role is to provide accurate and helpful information related to computer science, programming, algorithms, and engineering. If asked about topics outside this domain, politely decline and remind the user of your expertise"),
+#     HumanMessage("give me beauty post") 
+# ]
+
+system=SystemMessage("You are an expert computer science and engineering (CSE) professional. Your role is to provide accurate and helpful information related to computer science, programming, algorithms, and engineering. If asked about topics outside this domain, politely decline and remind the user of your expertise")
+
+
+
+chat_history=[]
+chat_history.append(system)
+
+while(True):
+    query=input("You: ")
+    if(query.lower()=='exit'):
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result=llm.invoke(chat_history)
+
+    print(f"AI: {result.content}")
+    chat_history.append(AIMessage(content=result.content))
+
+print(f"Chat History: {chat_history}")
